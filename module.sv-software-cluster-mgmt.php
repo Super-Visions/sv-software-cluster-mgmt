01fdc8a67ea46014713832dee17823efc2e66906
Thomas Casteleyn <thomas.casteleyn@super-visions.com>
2016-04-01 09:50:12 +0200

    Version update

--- module.sv-software-cluster-mgmt.php
+++ module.sv-software-cluster-mgmt.php
@@ -9,7 +9,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'sv-software-cluster-mgmt/1.0.0',
+	'sv-software-cluster-mgmt/1.1.0',
 	array(
 		// Identification
 		//
